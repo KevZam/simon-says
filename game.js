790c6fcb82ea9fbad94fba1af1fa5a003707ff4f
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-05-10 09:50:17 -0700

    finished basic functionality

--- game.js
+++ game.js
@@ -1,20 +1,80 @@
+let userClickedPattern = [];
 let gamePattern = [];
 let buttonColors = ["red", "blue", "green", "yellow"];
+let level = 0;
+let gameStarted = false;
 
 function nextSequence() {
+  level++;
+  $("h1").text("Level " + level);
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColors[randomNumber];
-  let colorSound = new Audio("sounds/" + randomChosenColour + ".mp3");
-  colorSound.play();
+
+  playSound(randomChosenColour);
   gamePattern.push(randomChosenColour);
+
   $("#" + randomChosenColour).addClass("pressed");
   setTimeout(function () {
     $("#" + randomChosenColour).removeClass("pressed");
   }, 300);
 }
 
-nextSequence();
+function playSound(name) {
+  let colourSound = new Audio("sounds/" + name + ".mp3");
+  colourSound.play();
+}
 
-$(document).click(function () {
-  nextSequence();
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+  } else {
+    let wrongSound = new Audio("sounds/wrong.mp3");
+    wrongSound.play();
+    startOver();
+    $("body").addClass("game-over");
+    $("h1").text("Game over, Press Any Key to Restart");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+  }
+
+  if (
+    userClickedPattern.length === gamePattern.length &&
+    gameStarted === true
+  ) {
+    setTimeout(function () {
+      nextSequence();
+      userClickedPattern = [];
+    }, 1000);
+  }
+}
+
+$(".btn").click(function (event) {
+  let userChosenColour = event.target.id;
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  userClickedPattern.push(userChosenColour);
+  let indexOfLastAnswer = userClickedPattern.length - 1;
+  checkAnswer(indexOfLastAnswer);
 });
+
+$(document).on("keydown", function () {
+  if (!gameStarted) {
+    nextSequence();
+  }
+  gameStarted = true;
+});
+
+function startOver() {
+  level = 0;
+  userClickedPattern = [];
+  gamePattern = [];
+  gameStarted = false;
+}
